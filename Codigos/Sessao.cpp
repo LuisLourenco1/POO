4b3f2f68c86bf3c9d12283396ff8821f34bcdfc4
Guidini1 <102241436+Guidini1@users.noreply.github.com>
2022-03-25 15:16:48 -0300

    Update Sessao.cpp

--- Codigos/Sessao.cpp
+++ Codigos/Sessao.cpp
@@ -1,10 +1,21 @@
 #include "Sessao.h"
 #include "DataHorario.h"
+#include <iostream>
+#include<string>
 
-Sessao::Sessao()
-{
+using namespace std;
+
+Sessao::Sessao(string nome_Peca, DataHorario&){
+  this->nome_Peca = nome_peca;   
 }
 
-Sessao::~Sessao()
-{
+string Sessao::getNome_Peca() const {
+        return this->nome_peca;
+ }
+
+Sessao::~Sessao(){
+  cout << "Chamada do destrutor" << endl;
 }
+
+string proximoLivre(){
+
