08fa67bdb52d8a745a7b86fd06088fa267e314c1
Guidini1 <102241436+Guidini1@users.noreply.github.com>
2022-03-25 15:16:52 -0300

    Update Sessão.h

--- Codigos/Sessão.h
+++ Codigos/Sessão.h
@@ -1,19 +1,27 @@
 #ifndef SESSAO_H
 #define SESSAO_H
 #include "DataHorario.h"
+#include <iostream>
+#include<string>
 
-class Sessao
-{
+using namespace std;
+
+namespace poo {
+	class Sessao{
 	public:
 		Sessao(string, DataHorario&);
 		~Sessao();
-		string proximoLivre()
-		bool verifica(string) 
-		bool ocupa(string, Pessoa&)
-		bool desocupa(string) 
-		int vagas()
+		int getNome_Peca() const;
+		string proximoLivre();
+		bool verifica(string);
+		bool ocupa(string, Pessoa&);
+		bool desocupa(string);
+		int vagas();
 		Operador << 
-	protected:
+	private:
+		string nome_peca;
+		int Poltronas[15][15];
+	}
 };
 
 #endif
